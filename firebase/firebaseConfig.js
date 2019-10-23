0f2595253d6f53f309bc297970ee62d732a9dac2
Saksham <gupta.10@iitj.ac.in>
2019-10-23 21:51:48 +0530

    Update firebaseConfig.js

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,4 +1,5 @@
 export const firebaseConfig = {
+<<<<<<< HEAD
     apiKey: "AIzaSyA8kNethrIbfPrw49tbuoYsL-FjrGsqhac",
     authDomain: "virestore-ae149.firebaseapp.com",
     databaseURL: "https://virestore-ae149.firebaseio.com",
@@ -6,4 +7,13 @@ export const firebaseConfig = {
     storageBucket: "",
     messagingSenderId: "797207289513",
     appId: "1:851100515728:web:f45f4442588cc905bbc715"
+=======
+    apiKey: "AIzaSyBiEZ2XykTMZm4KmAtu5xMzdqHo-UsXRXA",
+    authDomain: "doorzy-dev.firebaseapp.com",
+    databaseURL: "https://doorzy-dev.firebaseio.com",
+    projectId: "doorzy-dev",
+    storageBucket: "",
+    messagingSenderId: "797207289513",
+    appId: "1:797207289513:web:21bad304cc904949"
+>>>>>>> parent of cb548be... Merge pull request #3 from himanshudahiya/master
   };
