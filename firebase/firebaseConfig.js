17bcd847b9916bbd12e0669e9c871fb5930fade6
Saksham <gupta.10@iitj.ac.in>
2019-10-23 21:25:16 +0530

    Update firebaseConfig.js

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBiEZ2XykTMZm4KmAtu5xMzdqHo-UsXRXA",
-    authDomain: "doorzy-dev.firebaseapp.com",
-    databaseURL: "https://doorzy-dev.firebaseio.com",
-    projectId: "doorzy-dev",
+    apiKey: "AIzaSyA8kNethrIbfPrw49tbuoYsL-FjrGsqhac",
+    authDomain: "virestore-ae149.firebaseapp.com",
+    databaseURL: "https://virestore-ae149.firebaseio.com",
+    projectId: "virestore-ae49",
     storageBucket: "",
     messagingSenderId: "797207289513",
-    appId: "1:797207289513:web:21bad304cc904949"
+    appId: "1:851100515728:web:f45f4442588cc905bbc715"
   };
