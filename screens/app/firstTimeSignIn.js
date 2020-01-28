96f4097c7f5fc190e24e15bae5a1fc961d322470
Saksham <gupta.10@iitj.ac.in>
2020-01-28 05:44:59 +0530

    Added OTP verification

--- screens/app/firstTimeSignIn.js
+++ screens/app/firstTimeSignIn.js
@@ -4,7 +4,14 @@ e.g. what product? which brand? quantity? ....
 */
 
 import React, { Component } from "react";
-import { View, ScrollView, Text, Platform, StyleSheet } from "react-native";
+import {
+  View,
+  ScrollView,
+  Text,
+  Platform,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
 import { inject, observer } from "mobx-react/native";
 import firebase from "firebase";
 import "firebase/firestore";
@@ -15,16 +22,21 @@ import {
   createDrawerNavigator,
   createSwitchNavigator
 } from "react-navigation";
+import { Dimensions } from "react-native";
 import { colors } from "../../colors/colors";
 import { TextInput } from "react-native-gesture-handler";
 import { Button } from "react-native-elements";
-import generateInviteCode from '../../util/generateInviteCode';
-import registerForPushNotificationsAsync from '../../util/registerPushNotification';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import * as Segment from 'expo-analytics-segment';
+import { WebView } from "react-native-webview";
+import generateInviteCode from "../../util/generateInviteCode";
+import registerForPushNotificationsAsync from "../../util/registerPushNotification";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import * as Segment from "expo-analytics-segment";
 
+const notifId = require("uuid/v4");
+const captchaUrl = "https://virestore-ae149.firebaseapp.com/";
 
-const notifId = require('uuid/v4');
+export const DEVICE_WIDTH = Dimensions.get("window").width;
+export const DEVICE_HEIGHT = Dimensions.get("window").height;
 
 if (Platform.OS !== "web") {
   window = undefined;
@@ -40,7 +52,10 @@ class RegisterScreen extends Component {
       lname: "",
       phone: "",
       error: "",
-      loading : false
+      loading: false,
+      sendOtp: false,
+      verificationCode: "",
+      phoneSubmitted: false
     };
   }
 
@@ -70,126 +85,303 @@ class RegisterScreen extends Component {
         error: "Enter a valid 10-digit phone number."
       });
       return false;
-    }else {
-      this.setState({error:''});
+    } else {
+      this.setState({ error: "" });
       return true;
     }
   };
-
-   onSubmit = async () => {
+  // validatePhoneNumber = async () => {
+  //   var regexp = /^\+[0-9]?()[0-9](\s|\S)(\d[0-9]{8,16})$/
+  //   return regexp.test(this.state.phone)
+  // };
+  // handleVerifyCode = async () => {
+  //   const { confirmResult, verificationCode } = this.state;
+  //   if (verificationCode.length == 6) {
+  //     confirmResult
+  //       .confirm(verificationCode)
+  //       .then(this.onSubmit())
+  //       .catch(error => {
+  //         alert(error.message);
+  //         console.log(error);
+  //       });
+  //   } else {
+  //     alert("Please enter a 6 digit OTP code.");
+  //   }
+  //   console.log("yohoooo");
+  // };
+  detailSubmit = async () => {
     if (this.validate(this.state.fname, this.state.lname, this.state.phone)) {
-      this.setState({loading:true});
-      const db = firebase.firestore();
-      var uid = this.props.mainStore.uid;
-      var nid = notifId();
-      var notifBucket = db.collection('notifications').doc(uid).collection('notifs');
-      var rootRef = this.props.navigation;
-      var storeRef = this.props;
-      var thisRef = this;
-      const inviteCode = generateInviteCode(6);
-      const user = {
-        fname : this.state.fname,
-        lname : this.state.lname,
-        phone : this.state.phone,
-        inviteCode : inviteCode,
-        invitesLeft : 10,
-        regTimestamp : + new Date()
-      };
-      Segment.identifyWithTraits(uid,{
-        firstName: user.fname,
-        lastName: user.lname,
-        phone: user.phone,
-        createdAt: new Date(user.regTimestamp)
-      });
-      Segment.trackWithProperties('Sign Up',{
-        firstName: user.fname,
-        lastName: user.lname,
-        phone: user.phone
-      });
-      await registerForPushNotificationsAsync(uid);
+      const { phone } = this.state;
+      var newPhone = "+91" + phone;
+      this.setState({ phone: newPhone });
+      this.setState({ phoneSubmitted: true });
+    } else {
+      console.log(this.state.error);
+      this.setState({ loading: false });
+    }
+  };
+  testFunc = async event => {
+    const { phone } = this.state;
+    const message = event.nativeEvent.data;
+    switch (message) {
+      case "DOMLoaded":
+        this.webviewRef.injectJavaScript(`getToken('${phone}')`);
+        return;
+      case "ErrorSmsCode":
+        // SMS Not sent or Captcha verification failed. You can do whatever you want here
+        return;
+      case "":
+        return;
+      default: {
+        this.setState({
+          sendOtp: true,
+          verificationId: message
+        });
+      }
+    }
+    console.log("saksham");
+  };
+  // verifyOtp = async event => {
+  //   if (this.validate(this.state.fname, this.state.lname, this.state.phone)) {
+  //     const { phone } = this.state;
+  //     const message = event.nativeEvent.data;
 
-      await notifBucket.doc(nid).set({
-        id : nid,
-        timestamp : + new Date(),
-        read : false,
-        content : "Hi "+this.state.fname+", Welcome to the doorzy family. You are one of our first customers and you are special for us. I personally thank you for choosing doorzy as your doorstep delivery companion. Also, we are open to feedbacks and appreciate any kind of feedback or criticism.",
-        title : "Welcome aboard!"
-      }).catch(function(err){
-        this.setState({loading:false});
+  //     switch (message) {
+  //       case "DOMLoaded":
+  //         this.webviewRef.injectJavaScript(`getToken('${phone}')`);
+  //         return;
+  //       case "ErrorSmsCode":
+  //         // SMS Not sent or Captcha verification failed. You can do whatever you want here
+  //         return;
+  //       case "":
+  //         return;
+  //       default: {
+  //         this.setState({
+  //           promptSmsCode: true,
+  //           verificationId: message
+  //         });
+  //       }
+  //     }
+
+  //     // firebase
+  //     //   .auth()
+  //     //   .signInWithPhoneNumber(this.state.phone)
+  //     //   .then(confirmResult => {
+  //     //     this.setState({ confirmResult });
+  //     //   })
+  //     //   .catch(error => {
+  //     //     alert(error.message);
+  //     //     console.log(error);
+  //     //   });
+  //     this.setState({ sendOtp: true });
+  //   } else {
+  //     console.log(this.state.error);
+  //     this.setState({ loading: false });
+  //   }
+  // };
+  onSignIn = async () => {};
+  onSubmit = async () => {
+    this.setState({ loading: true });
+    const db = firebase.firestore();
+    var uid = this.props.mainStore.uid;
+    var nid = notifId();
+    console.log("reached here");
+    const { verificationCode, verificationId } = this.state;
+    const credential = firebase.auth.PhoneAuthProvider.credential(
+      verificationId,
+      verificationCode
+    );
+    var notifBucket = db
+      .collection("notifications")
+      .doc(uid)
+      .collection("notifs");
+    var rootRef = this.props.navigation;
+    var storeRef = this.props;
+    var thisRef = this;
+    const inviteCode = generateInviteCode(6);
+    const user = {
+      fname: this.state.fname,
+      lname: this.state.lname,
+      phone: this.state.phone,
+      inviteCode: inviteCode,
+      invitesLeft: 10,
+      regTimestamp: +new Date()
+    };
+    Segment.identifyWithTraits(uid, {
+      firstName: user.fname,
+      lastName: user.lname,
+      phone: user.phone,
+      createdAt: new Date(user.regTimestamp)
+    });
+    Segment.trackWithProperties("Sign Up", {
+      firstName: user.fname,
+      lastName: user.lname,
+      phone: user.phone
+    });
+    await registerForPushNotificationsAsync(uid);
+
+    await notifBucket
+      .doc(nid)
+      .set({
+        id: nid,
+        timestamp: +new Date(),
+        read: false,
+        content:
+          "Hi " +
+          this.state.fname +
+          ", Welcome to the doorzy family. You are one of our first customers and you are special for us. I personally thank you for choosing doorzy as your doorstep delivery companion. Also, we are open to feedbacks and appreciate any kind of feedback or criticism.",
+        title: "Welcome aboard!"
+      })
+      .catch(function(err) {
+        this.setState({ loading: false });
         console.log(err);
       });
 
-      await db.collection("inviteCodes").doc(uid).set({code : inviteCode,uid : uid}).then(()=>{
-        
-      }).catch(err=>{console.log(err);this.setState({loading:false});});
+    await db
+      .collection("inviteCodes")
+      .doc(uid)
+      .set({ code: inviteCode, uid: uid })
+      .then(() => {})
+      .catch(err => {
+        console.log(err);
+        this.setState({ loading: false });
+      });
 
-      await db.collection('users').doc(uid).set(user,{merge:true}).then(function(){
+    await db
+      .collection("users")
+      .doc(uid)
+      .set(user, { merge: true })
+      .then(function() {
         storeRef.mainStore.setUser(user);
-        rootRef.navigate('App');
-      }).catch(err =>{console.log(err);this.setState({loading:false});});
-
-    } else {
-      console.log(this.state.error);
-      this.setState({loading:false});
-    }
+        rootRef.navigate("App");
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ loading: false });
+      });
   };
 
   render() {
+    const { phoneSubmitted, sendOtp, phone } = this.state;
+    if (!phoneSubmitted)
+      return (
+        <KeyboardAwareScrollView enableOnAndroid={true}>
+          <ScrollView contentContainerStyle={styles.body}>
+            <View style={styles.formTitleWrapper}>
+              <Text style={styles.h3}>
+                Welcome to doorzy{" "}
+                <Text style={styles.secondaryColor}>FAMILY.</Text>
+              </Text>
+            </View>
+            <View style={styles.formContainer}>
+              <View style={styles.textInputWrapper}>
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="First name"
+                  value={this.state.fname}
+                  onChangeText={text => {
+                    this.setState({ fname: text });
+                  }}
+                />
+              </View>
+              <View style={styles.textInputWrapper}>
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="Last name"
+                  value={this.state.lname}
+                  onChangeText={text => {
+                    this.setState({ lname: text });
+                  }}
+                />
+              </View>
+              <View style={styles.textInputWrapper}>
+                <TextInput
+                  style={styles.textInput}
+                  placeholder="Mobile Number"
+                  value={this.state.phone}
+                  onChangeText={text => {
+                    this.setState({ phone: text });
+                  }}
+                />
+              </View>
+              <View style={styles.textInputWrapper}>
+                <Button
+                  buttonStyle={styles.btn}
+                  type="solid"
+                  raised={false}
+                  onPress={this.detailSubmit}
+                  title="Next"
+                  loading={this.state.loading}
+                  disabled={this.state.loading}
+                />
+              </View>
+            </View>
+            <View style={styles.errorContainer}>
+              <Text style={styles.error}>{this.state.error}</Text>
+            </View>
+          </ScrollView>
+        </KeyboardAwareScrollView>
+      );
+    if (!sendOtp)
+      return (
+        <KeyboardAwareScrollView enableOnAndroid={true}>
+          <ScrollView contentContainerStyle={styles.body}>
+            <View style={styles.formTitleWrapper}>
+              <Text style={styles.h3}>
+                Welcome to doorzy{" "}
+                <Text style={styles.secondaryColor}>FAMILY.</Text>
+              </Text>
+            </View>
+            <WebView
+              injectedJavaScript={`getToken('${phone}')`}
+              ref={r => (this.webviewRef = r)}
+              source={{ uri: captchaUrl }}
+              style={styles.video}
+              onMessage={this.testFunc}
+            />
+            <View style={styles.errorContainer}>
+              <Text style={styles.error}>{this.state.error}</Text>
+            </View>
+          </ScrollView>
+        </KeyboardAwareScrollView>
+      );
     return (
       <KeyboardAwareScrollView enableOnAndroid={true}>
-      <ScrollView contentContainerStyle={styles.body}>
-        <View style={styles.formTitleWrapper}>
-          <Text style={styles.h3}>
-            Welcome to doorzy <Text style={styles.secondaryColor}>FAMILY.</Text>
-          </Text>
-        </View>
-        <View style={styles.formContainer}>
+        <ScrollView contentContainerStyle={styles.body}>
+          <View style={styles.formTitleWrapper}>
+            <Text style={styles.h3}>
+              Welcome to doorzy{" "}
+              <Text style={styles.secondaryColor}>FAMILY.</Text>
+            </Text>
+          </View>
+
           <View style={styles.textInputWrapper}>
             <TextInput
               style={styles.textInput}
-              placeholder="First name"
-              value={this.state.fname}
-              onChangeText={text => {
-                this.setState({ fname: text });
+              placeholder="Verification code"
+              value={this.state.verificationCode}
+              keyboardType="numeric"
+              onChangeText={verificationCode => {
+                this.setState({ verificationCode });
               }}
+              maxLength={6}
             />
           </View>
-          <View style={styles.textInputWrapper}>
-            <TextInput
-              style={styles.textInput}
-              placeholder="Last name"
-              value={this.state.lname}
-              onChangeText={text => {
-                this.setState({ lname: text });
-              }}
-            />
-          </View>
-          <View style={styles.textInputWrapper}>
-            <TextInput
-              style={styles.textInput}
-              placeholder="Mobile Number"
-              value={this.state.phone}
-              onChangeText={text => {
-                this.setState({ phone: text });
-              }}
-            />
-          </View>
-          <View style={styles.textInputWrapper}>
-            <Button
-              buttonStyle={styles.btn}
-              type="solid"
-              raised={false}
-              onPress={this.onSubmit}
-              title="Next"
-              loading={this.state.loading}
-              disabled={this.state.loading}
-            />
-          </View>
+
+          <Button
+            buttonStyle={styles.btn}
+            type="solid"
+            raised={false}
+            onPress={this.onSubmit}
+            title="Verify"
+            loading={this.state.loading}
+            disabled={this.state.loading}
+          />
+
           <View style={styles.errorContainer}>
             <Text style={styles.error}>{this.state.error}</Text>
           </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
       </KeyboardAwareScrollView>
     );
   }
@@ -216,6 +408,13 @@ const styles = StyleSheet.create({
     backgroundColor: colors.brandPrimary,
     flex: 1,
     justifyContent: "center"
+  },
+  video: {
+    flex: 1,
+    width: DEVICE_WIDTH,
+    height: 800,
+    position: "relative",
+    top: 0
   },
   h3: {
     fontFamily: "Rubik-Regular",
